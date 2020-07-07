905b724283a6510835ddb9269d055c5a8da81b97
Ankur <chourasia.ankur1@gmail.com>
2020-07-08 03:46:32 +0530

    Removed few imports

--- app.py
+++ app.py
@@ -2,8 +2,6 @@
 import pandas as pd
 import numpy as np
 import plotly.express as px
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 
